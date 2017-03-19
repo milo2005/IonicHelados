32e55c7c948d70b6cdd28ba566534af1a1dbb6ba
Dario Chamorro <dario.chamorro.v@gmail.com>
2017-03-19 16:19:16 -0500

    AddHelado UI implemented

--- src/pages/add-helado/add-helado.ts
+++ src/pages/add-helado/add-helado.ts
@@ -3,12 +3,6 @@ import { NavController, NavParams } from 'ionic-angular';
 
 import { Helado, HeladosData } from '../../providers/helados-data';
 
-/*
-  Generated class for the AddHelado page.
-
-  See http://ionicframework.com/docs/v2/components/#navigation for more info on
-  Ionic pages and navigation.
-*/
 @Component({
   selector: 'page-add-helado',
   templateUrl: 'add-helado.html'
@@ -18,10 +12,14 @@ export class AddHeladoPage {
   helado: Helado;
 
   constructor(public navCtrl: NavController, public navParams: NavParams, public service: HeladosData) { 
-    
+    this.helado =  new Helado()
   }
 
   ionViewDidLoad() {
+
+  }
+
+  save() {
 
   }
 
